4822faa9432ec54cf13b1b6f4c1b70f326e45418
mesfint <mesfin_meng@yahoo.com>
2021-12-01 19:37:22 +0200

    book details added

--- src/pages/book/Lists.tsx
+++ src/pages/book/Lists.tsx
@@ -61,9 +61,6 @@ const BookList: VoidFunctionComponent = () => {
                     <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Title
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                      Author
-                    </th>
 
                     <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Published Year
@@ -93,9 +90,6 @@ const BookList: VoidFunctionComponent = () => {
                       </Link>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap  ">
                         {book.title}
-                      </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap  ">
-                        {'Author'}
                       </td>
 
                       <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap ">
