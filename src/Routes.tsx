a67c482877beae79fb3d85a7fdb8606bb3e906f1
mesfint <mesfin_meng@yahoo.com>
2021-12-21 16:09:09 +0200

    router fixed

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,6 @@
 import React from 'react'
 // import { Switch, Route } from 'react-router-dom' // old
 import { Routes as Switch, Route } from 'react-router-dom' // updated to latest
-<<<<<<< .merge_file_QcecnZ
-import type { PathRouteProps } from 'react-router'
-//https://reactrouter.com/docs/en/v6/upgrading/v5
-//https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
-
-import Home from './pages/Home'
-import AddBook from './components/Form/AddBook'
-import Books from './components/Books/Books'
-import AddAuthors from './components/Form/AddAuthors'
-import Authors from './components/Authors/Authors'
-import Product from './pages/Product'
-=======
 import BookList from './pages/book/Lists'
 import type { PathRouteProps } from 'react-router'
 import Home from './pages/Home'
@@ -68,36 +56,12 @@ export const routerList: PathRouterType[] = [
     title: 'Borrow book',
   },
 ]
->>>>>>> .merge_file_NQJiR5
-
-type PathType = PathRouteProps & {
-  title: string
-}
-
-export const RouteList: PathType[] = []
-export const routerList: PathType[] = [
-  { path: '/', element: <Home />, title: 'Home' },
-  { path: '/products/:id', element: <Product />, title: 'Product' },
-  { path: '/books', element: <Books />, title: 'Books' },
-  { path: '/books/add', element: <AddBook />, title: 'Add new book' },
-  { path: '/books/add', element: <Authors />, title: 'Add new book' },
-  { path: '/books/add', element: <AddAuthors />, title: 'Add new book' },
-]
 
 const Routes = () => (
   <Switch>
-<<<<<<< .merge_file_QcecnZ
-    <Route path="/" element={<Home />} />
-    <Route path="/AddBook" element={<AddBook />} />
-    <Route path="/Books" element={<Books />} />
-    <Route path="/Authors" element={<Authors />} />
-    <Route path="/AddAuthors" element={<AddAuthors />} />
-    <Route path="/products/:id" element={<Product />} />
-=======
     {routerList.map((route, index) => (
       <Route key={index} path={route.path} element={route.element} />
     ))}
->>>>>>> .merge_file_NQJiR5
   </Switch>
 )
 
